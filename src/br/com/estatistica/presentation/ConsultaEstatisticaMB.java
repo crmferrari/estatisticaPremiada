86c11be5043655c8f645a99febf8cb94926bb3d1
Home <Home@192.168.1.3>
2015-12-06 20:58:19 -0300

    Implementando estatística 1.

---
+++ src/br/com/estatistica/presentation/ConsultaEstatisticaMB.java
@@ -0,0 +1,21 @@
+package br.com.estatistica.presentation;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import br.com.estatistica.business.IManterResultadoService;
+import br.com.estatistica.common.entity.Resultado;
+import org.primefaces.model.chart.BarChartModel;
+import org.primefaces.model.chart.ChartSeries;
+
+@ManagedBean(eager = true)
+@RequestScoped
+public class ConsultaEstatisticaMB {
+	
+	private BarChartModel barModel;
+	
+	
+	
+	
+	
+	
+}
